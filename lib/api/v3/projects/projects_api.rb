36a94489dfa5146251c7a7fc9b5cfee3299b51ea
ulferts <jens.ulferts@googlemail.com>
2025-09-24 12:27:11 +0200

    move project create from to workspace

--- lib/api/v3/projects/projects_api.rb
+++ lib/api/v3/projects/projects_api.rb
@@ -70,8 +70,6 @@ module API
             delete &::API::V3::Utilities::Endpoints::Delete.new(model: Project,
                                                                 process_service: ::Projects::ScheduleDeletionService)
                                                            .mount
-
-            mount ::API::V3::Projects::UpdateFormAPI
             mount API::V3::Projects::Copy::CopyAPI
 
             mount ::API::V3::Workspaces::NestedApis
